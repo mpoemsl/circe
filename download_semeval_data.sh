5c560391299395942b0e73cd2a3e059c15159842
mpoemsl <37959974+mpoemsl@users.noreply.github.com>
2020-04-26 15:53:30 +0200

    Removed truth and targets duplication

--- download_semeval_data.sh
+++ download_semeval_data.sh
@@ -12,8 +12,6 @@ unzip datasets/en-semeval.zip
 
 mv semeval2020_ulscd_eng/corpus1/lemma/ccoha1.txt.gz datasets/en-semeval/c1.txt.gz
 mv semeval2020_ulscd_eng/corpus2/lemma/ccoha2.txt.gz datasets/en-semeval/c2.txt.gz
-mv semeval2020_ulscd_eng/truth/graded.txt datasets/en-semeval/truth.tsv
-mv semeval2020_ulscd_eng/targets.txt datasets/en-semeval/targets.tsv
 
 rm -r semeval2020_ulscd_eng/
 
@@ -26,8 +24,6 @@ unzip datasets/de-semeval.zip
 
 mv semeval2020_ulscd_ger/corpus1/lemma/dta.txt.gz datasets/de-semeval/c1.txt.gz
 mv semeval2020_ulscd_ger/corpus2/lemma/bznd.txt.gz datasets/de-semeval/c2.txt.gz
-mv semeval2020_ulscd_ger/truth/graded.txt datasets/de-semeval/truth.tsv
-mv semeval2020_ulscd_ger/targets.txt datasets/de-semeval/targets.tsv
 
 rm -rf semeval2020_ulscd_ger/
 
@@ -40,8 +36,6 @@ unzip datasets/ln-semeval.zip
 
 mv semeval2020_ulscd_lat/corpus1/lemma/LatinISE1.txt.gz datasets/ln-semeval/c1.txt.gz
 mv semeval2020_ulscd_lat/corpus2/lemma/LatinISE2.txt.gz datasets/ln-semeval/c2.txt.gz
-mv semeval2020_ulscd_lat/truth/graded.txt datasets/ln-semeval/truth.tsv
-mv semeval2020_ulscd_lat/targets.txt datasets/ln-semeval/targets.tsv
 
 rm -rf semeval2020_ulscd_lat/
 
@@ -54,8 +48,6 @@ unzip datasets/sw-semeval.zip
 
 mv semeval2020_ulscd_swe/corpus1/lemma/kubhist2a.txt.gz datasets/sw-semeval/c1.txt.gz
 mv semeval2020_ulscd_swe/corpus2/lemma/kubhist2b.txt.gz datasets/sw-semeval/c2.txt.gz
-mv semeval2020_ulscd_swe/truth/graded.txt datasets/sw-semeval/truth.tsv
-mv semeval2020_ulscd_swe/targets.txt datasets/sw-semeval/targets.tsv
 
 rm -rf semeval2020_ulscd_swe/
 
